f98cf3b07909607e0534296aff832e3ab39e900a
Scott Gaines <scottdgaines@gmail.com>
2023-01-05 11:54:36 -0700

    Add testing suite for Dashboard

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,13 +3,14 @@ import cleanData from './utilities'
 const KEY = process.env.REACT_APP_API_KEY
 
 const fetchData = async (category) => {
+    console.log('category', category)
     let url = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${KEY}`
-
+    console.log('url', url)
     try {
         const response = await fetch(url)
+        console.log('resp', response.json)
         const data = await response.json()
         const cleanedArticles = cleanData(data)
-
         if (!response.ok) {
             return
         }
